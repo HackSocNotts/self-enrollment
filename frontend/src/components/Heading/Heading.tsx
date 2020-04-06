2114060e723dd7a3b2651ff21154e3987aba7049
Aaron Osher <git@aaronosher.io>
2020-04-07 00:21:09 +0100

    Add a this is very much not production message

--- frontend/src/components/Heading/Heading.tsx
+++ frontend/src/components/Heading/Heading.tsx
@@ -7,16 +7,22 @@ const Heading: React.FC = () => {
   const classes = useStyles();
 
   return (
-    <div className={classes.heroContent}>
-      <Container maxWidth="sm">
-        <Typography component="h1" variant="h3" align="center" color="textPrimary" gutterBottom>
-          Self Enrollment Service
-        </Typography>
-        <Typography variant="h5" align="center" color="textSecondary" paragraph>
-          Use this service to self-enroll yourself on third party services that require you to hold a separate account.
-        </Typography>
-      </Container>
-    </div>
+    <>
+      <Typography component="p" align="left" color="textSecondary" gutterBottom>
+        This service is in beta and is likely to have bugs. If it breaks, please shout at Aaron. Thank, bye.
+      </Typography>
+      <div className={classes.heroContent}>
+        <Container maxWidth="sm">
+          <Typography component="h1" variant="h3" align="center" color="textPrimary" gutterBottom>
+            Self Enrollment Service
+          </Typography>
+          <Typography variant="h5" align="center" color="textSecondary" paragraph>
+            Use this service to self-enroll yourself on third party services that require you to hold a separate
+            account.
+          </Typography>
+        </Container>
+      </div>
+    </>
   );
 };
 

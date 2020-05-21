080edd3fd324d203acd2a08c7db13dc4cb9b2756
Aaron Osher <git@aaronosher.io>
2020-05-21 05:35:12 +0100

    Add GitHub

--- src/config.ts
+++ src/config.ts
@@ -22,3 +22,10 @@ export const AAD_CLIENT_SECRET = process.env.AAD_CLIENT_SECRET;
 export const AAD_TENNANT_ID = process.env.AAD_TENNANT_ID;
 export const AAD_ISSUER = process.env.AAD_ISSUER;
 export const AAD_APPIDURI = process.env.AAD_APPIDURI;
+
+//GitHub
+export const GITHUB_APP_ID = process.env.GITHUB_APP_ID as string;
+export const GITHUB_PRIVATE_KEY = process.env.GITHUB_PRIVATE_KEY as string;
+export const GITHUB_INSTALLATION_ID = process.env.GITHUB_INSTALLATION_ID as string;
+export const GITHUB_CLIENT_ID = process.env.GITHUB_CLIENT_ID as string;
+export const GITHUB_CLIENT_SECRET = process.env.GITHUB_INSTALLATION_ID as string;

27bac39df468b28619d8a41c24a5761b5fa79ed8
Aaron Osher <git@aaronosher.io>
2020-05-25 16:00:27 +0100

    Update GitHub Service & Config
    Add checkmembership
    Renamed class to GitHubService
    Add GITHUB_ORGANISATION to config

--- src/app/services/GitHub/GitHub.ts
+++ src/app/services/GitHub/GitHub.ts
@@ -8,14 +8,16 @@ import {
   GITHUB_CLIENT_ID,
   GITHUB_CLIENT_SECRET,
   GITHUB_INSTALLATION_ID,
+  GITHUB_ORGANISATION,
   GITHUB_PRIVATE_KEY,
 } from '../../../config';
+import { NO_CONTENT, NOT_FOUND } from 'http-status-codes';
 import { createAppAuth } from '@octokit/auth-app';
 import { InstallationAccessTokenData } from '@octokit/auth-app/dist-types/types';
 import { Octokit } from '@octokit/rest';
 import { Optional } from '../../../utils/types';
 
-class GitHub {
+class GitHubService {
   private readonly app: Octokit;
   private token: Optional<string>;
 
@@ -41,9 +43,29 @@ class GitHub {
     }
   }
 
+  public async checkMembership(username: string) {
+    try {
+      const { status } = await this.app.orgs.checkMembership({
+        org: GITHUB_ORGANISATION,
+        username,
+      });
+
+      if (status === NO_CONTENT) {
+        return true;
+      }
+
+      throw new Error('Unexpected response');
+    } catch (e) {
+      if (e.status === NOT_FOUND) {
+        return false;
+      }
+      throw e;
+    }
+  }
+
   public getToken() {
     return this.token;
   }
 }
 
-export default GitHub;
+export default GitHubService;

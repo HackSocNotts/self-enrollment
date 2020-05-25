e740d92f168b164a63d420ebbf7e484bed137af6
Aaron Osher <git@aaronosher.io>
2020-05-25 21:27:59 +0100

    Add GitHub Team Retrieval

--- src/app/controllers/github.ts
+++ src/app/controllers/github.ts
@@ -7,6 +7,7 @@ import { BAD_REQUEST, INTERNAL_SERVER_ERROR, OK } from 'http-status-codes';
 import { ClassMiddleware, Controller, Get } from '@overnightjs/core';
 import GitHubService, { GitHubOauthService } from '../services/GitHub';
 import { Request, Response } from 'express';
+import Database from '../services/Database';
 import isAuthenticated from '../middleware/isAuthenticated';
 import { Logger } from '@overnightjs/logger';
 
@@ -61,6 +62,56 @@ class GitHubController {
       return res
         .status(INTERNAL_SERVER_ERROR)
         .send('Server Error Occured.')
+        .end();
+    }
+  }
+
+  private async teams(jobTitle: string, groups: string[]) {
+    const db = Database.getInstance();
+    try {
+      const positionTeam = (await db.getPositionGroup(jobTitle)).map(group => ({
+        slug: group.gitHubTeam,
+      }));
+      const roleTeam = (await db.findGroups(groups)).map(group => ({
+        slug: group.gitHubTeamSlug,
+      }));
+
+      return [...positionTeam, ...roleTeam].filter(team => !!team.slug) as {
+        slug: string;
+      }[];
+    } catch (e) {
+      throw e;
+    }
+  }
+
+  @Get('teams')
+  public async getTeams(req: Request, res: Response) {
+    if (!req.session) {
+      return res
+        .status(INTERNAL_SERVER_ERROR)
+        .send('Session failed to initialize.')
+        .end();
+    }
+
+    if (!req.session.groups && !req.session.jobTitle) {
+      return res
+        .status(BAD_REQUEST)
+        .send('Missing Azure AD keys')
+        .end();
+    }
+
+    try {
+      const teams = (await this.teams(req.session.jobTitle, req.session.groups)).map(team => team.slug);
+
+      return res
+        .status(OK)
+        .json({ teams })
+        .end();
+    } catch (e) {
+      Logger.Err(e, true);
+      return res
+        .status(INTERNAL_SERVER_ERROR)
+        .send('Unknown Error Occurred')
         .end();
     }
   }

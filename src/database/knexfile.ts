1d28226ab83d361af0516f8107af8708087b459c
Aaron Osher <git@aaronosher.io>
2021-05-27 03:19:31 +0100

    Update knexfile.ts
    add ssl support

--- src/database/knexfile.ts
+++ src/database/knexfile.ts
@@ -16,6 +16,7 @@ export const connection = {
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
+  ssl: (process.env.DB_SSL === 'true'),
   options: {
     port: process.env.DB_PORT,
     enableArithAbort: true,
